bfe0ce78ce8f5f32ab04f56c3e0e98c077c271b7
roridemonslay <olaniyideborah63@gmail.com>
2026-07-13 22:07:39 -0400

    UPDATE ON START_TIME

--- src/shared/models.py
+++ src/shared/models.py
@@ -3,12 +3,22 @@
 example_vision_verdict = {
     "chord_shape": "G", #the chord it thinks im making 
     "confidence" : 0.87, # how sure it is that im making this chord 
-    "timestamp" : 12.43, # how many seconds into it the song it's been
+    "start_time" : 12.43, # how many seconds into it the song it's been
 }
 
 example_sound_verdict ={
     "sound_pick_up" :"F#", #the sound it picked up was this
     "confidence" : 0.87,   #how confident it's this sound
-    "timestamp": 13.45, 
+    "start_time": 13.45, 
     "volume" : 20.67, 
+}
+example_chord_chart = { 
+    "sequence":[
+        {"chord": "G", "start_time": 2.5},
+        {"chord": "F#", "start_time": 3.5},
+        {"chord": "D#", "start_time": 4.5},
+
+    ], 
+    
+
 }
