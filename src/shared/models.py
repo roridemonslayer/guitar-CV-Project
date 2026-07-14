aff077cabe9fc649605cbf966c156ff6c00febd1
roridemonslay <olaniyideborah63@gmail.com>
2026-07-14 11:25:11 -0400

    FEAT: built out model skeleton for other files to call on, will be tweaking as i go on

--- src/shared/models.py
+++ src/shared/models.py
@@ -3,16 +3,19 @@
 example_vision_verdict = {
     "chord_shape": "G", #the chord it thinks im making 
     "confidence" : 0.87, # how sure it is that im making this chord 
-    "start_time" : 12.43, # how many seconds into it the song it's been
+    "timestamp" : 12.43, # how many seconds into it the song it's been
 }
 
 example_sound_verdict ={
     "sound_pick_up" :"F#", #the sound it picked up was this
     "confidence" : 0.87,   #how confident it's this sound
-    "start_time": 13.45, 
+    "timestamp": 13.45, 
     "volume" : 20.67, 
 }
 example_chord_chart = { 
+    "title" : "The cute", 
+    "artist": "Olivia Rodrigo", 
+    "mode": "wait", #here it doesn't more when it's on wait till the chord is strung then "march" moves 
     "sequence":[
         {"chord": "G", "start_time": 2.5},
         {"chord": "F#", "start_time": 3.5},
