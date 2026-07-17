cdbfa6909b382f1685e0533fc90492b6c4bdc9cc
roridemonslay <olaniyideborah63@gmail.com>
2026-07-17 02:32:50 -0400

    i js pulling ifno from that data to see if the chord ur playin g mstchrd eiyht whts stores

---
+++ src/session/scorer.py
@@ -0,0 +1,8 @@
+from src.shared.models import example_sound_verdict, example_chord_chart
+
+def is_chord_correct(target_chord example_sound_verdict, example_chord_chart): 
+    vision_chord = vision_verdict["chord_shape"]
+    sound_note = sound_verdict["sound_pick_up"]
+
+
+    return False
