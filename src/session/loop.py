82486b04c76eeca62d1899ca694af4e5361b7dfe
roridemonslay <olaniyideborah63@gmail.com>
2026-07-31 12:22:19 -0400

    completed game coutdown and chart walking

--- src/session/loop.py
+++ src/session/loop.py
@@ -1,11 +1,9 @@
 import time #this is being used for the countdown 
-
+from src.shared.models import example_chord_chart
 for num in [3,2,1]:
     print (num)
     time.sleep(1)#pauses at the one second point doesn't move
 print("Go!")
 
-
-
-
-
+for entry in example_chord_chart["sequence"]:
+    print(f"Play: {entry['chord']}")
