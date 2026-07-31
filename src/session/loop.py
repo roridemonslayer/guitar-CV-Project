9d8904c2400fb63e4a796d8368895f028c692699
roridemonslay <olaniyideborah63@gmail.com>
2026-07-31 11:50:00 -0400

    game timer

--- src/session/loop.py
+++ src/session/loop.py
@@ -1,9 +1,9 @@
 import time #this is being used for the countdown 
-time.sleep(1) #pauses at the one second point doesn't move
 
 for num in [3,2,1]:
     print (num)
-    time.sleep(1)
+    time.sleep(1)#pauses at the one second point doesn't move
+print("Go!")
 
 
 
