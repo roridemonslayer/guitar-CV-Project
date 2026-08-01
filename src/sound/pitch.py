8a174c60fe58ba5de53afb2aded1f0f51962b388
roridemonslay <olaniyideborah63@gmail.com>
2026-08-01 19:10:32 -0400

    sound pipline

--- src/sound/pitch.py
+++ src/sound/pitch.py
@@ -1,4 +1,5 @@
-import numpy as np 
+import numpy as np
+import time  #this is needed for the timestamp
 
 def detect_freq(audio, sample_rate = 44100): #this is the default value 
     spectrum = np.abs(np.fft.rfft(audio)) # rfft does all of the unmixing audio over a set tumne = unmixing. this js makes all the outputs positive. this outputs where each slot ois how present the requency wa s
@@ -6,13 +7,36 @@ def detect_freq(audio, sample_rate = 44100): #this is the default value
     peak_index = np.argmax(spectrum) #this returns which slot holds the biggest value. so if slot is 220 then the asnwer is 220 
     return freqs[peak_index]
 
-NOTE_NAME = ["C", "C#", "D","D#","E","F","F#","G","A","A#","B"]
+NOTE_NAME = ["C", "C#", "D","D#","E","F","F#","G","G#","A","A#","B"]
 
 
 def freq_to_note(freq):
     semitones_from_a4 = 12 *np.log2(freq/440.0)
     note_index = round(semitones_from_a4) + 9 
     return NOTE_NAME[note_index % 12]
+
+def get_sound_verdict():
+    from src.sound.capture import record_chunk #pulls the recording function
+    audio = record_chunk() #one sec of mic data, 44,100 numbers 
+    spectrum = np.abs(np.fft.rfft(audio)) # this pulls the 
+    freqs = np.fft.rfftfreq(len(audio), 1/44100)# the hz score same as the last one 
+    peak_index = np.argmax(spectrum) #which slot is the loudest 
+    return {
+        "sound_pick_up":freq_to_note(freqs[peak_index]),
+        "confidence":float(spectrum[peak_index] / spectrum.sum()),
+        "timestamp":time.time(),
+        "volume": float(np.abs(audio).mean()), 
+    }
+    
+     
+
+
+
+
+
+
+
+
 
 #this whole line js take sa nfumer liek 330 and turn it to note. "C"
 if __name__ == "__main__":
@@ -22,3 +46,4 @@ if __name__ == "__main__":
     freq = detect_freq(audio)
     print("Frequency:", freq)
     print("Note:", freq_to_note(freq))
+    print(get_sound_verdict())
